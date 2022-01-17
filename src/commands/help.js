f62914b667da9a0fd8a4d29389df6e52b92be8b0
ImpassiveMoon03 <offordjr129@gmail.com>
2022-01-17 15:01:57 -0500

    Fixed typos in help command

--- src/commands/help.js
+++ src/commands/help.js
@@ -16,8 +16,8 @@ module.exports = {
                 .setTitle("Tic Tac Toe Commands")
                 .setColor('#b00b1e')
                 .addField(`<:slash:873820777932279828>help`, 'View the help embed')
-                .addField(`<:slash:873820777932279828>game <member>`, "Start a game with the selected memeber\n**Member** - The member you wish to challenge")
-                .addField(`<:slash:873820777932279828>move <square>`, "Take your turne\n**Square** - A valid space on the board")
+                .addField(`<:slash:873820777932279828>game <member>`, "Start a game with the selected member\n**Member** - The member you wish to challenge")
+                .addField(`<:slash:873820777932279828>move <square>`, "Take your turn\n**Square** - A valid space on the board")
                 .addField(`<:slash:873820777932279828>end`, "End your current game")
                 .addField(`<:slash:873820777932279828>board`, "View your current game's board")
                 .addField(`<:slash:873820777932279828>how`, "Get a small how-to of tic-tac-toe")
@@ -25,7 +25,7 @@ module.exports = {
                 .addField(`<:slash:873820777932279828>stats`, "View some stats on the bot")
                 .addField(`<:slash:873820777932279828>profile {member}`, "View a member stats or your own!\n**Member** - The member who's stats you want to view")
                 .addField(`<:slash:873820777932279828>changelog {version}`, "View the change log\n**Version** - The specific version you want to view the changes for")
-                .setDescription("Want to support development? [Upote the bot on top.gg](https://top.gg/bot/762833969183326228/vote)")
+                .setDescription("Want to support development? [Upvote the bot on top.gg](https://top.gg/bot/762833969183326228/vote)")
                 .setFooter('<> - Required Arguments || {} - Optional Arguments')
             ]
         }).then(() => {
